99bb9b28d26ebf1eda752cdf4ec53ad5f9994931
satyars14 <60698678+satyars14@users.noreply.github.com>
2020-02-16 00:40:06 +0530

    update-med

---
+++ med-2/header.php
@@ -0,0 +1,33 @@
+<header id="myHeader">
+    <div class="container">
+        <div class="row clearfix">
+            <div class="col-lg-3 logo">
+                    
+                    <a href="index.php"><img src="images/logo1.png" alt="My Medical Mate" title="My Medical Mate"></a>
+            </div>
+            <div class="col-lg-9">
+                <!--div class="call-to-action">
+                    <ul class="list-inline">
+                        <li class="list-inline-item"><a class="phone" href="tel:+917986451320"><i class="fa fa-phone"></i> <span>+91 (798) 645-1320</span></a></li>
+                        <li class="list-inline-item"><a href="mailto:info@mymedicalmate.com"><i class="fa fa-envelope"></i> <span>info@mymedicalmate.com</span></a></li>
+                        <li class="list-inline-item"><a href="#"><i class="fa fa-sign-in"></i> <span>Login</span></a></li>
+                        <li class="list-inline-item"><a href="#"><i class="fa fa-user-plus"></i> <span>Register</span></a></li>
+                    </ul>
+
+                </div-->
+                <nav class="mb-nav">
+                        <ul class="list-inline">
+                            <li><a href="#"><img src="images/icons/nav/prescription.png" alt="" class="nav-icon">Prescription</a></li>
+                            <li><a href="#"><img src="images/icons/nav/doctor.png" alt="" class="nav-icon">Doctors</a></li>
+                            <li><a href="#"><img src="images/icons/nav/clinic.png" alt="" class="nav-icon">Clinic</a></li>
+                            <li><a href="#"><img src="images/icons/nav/assistant.png" alt="" class="nav-icon">Get Assistants</a></li>
+                            <li><a href="#">Login</a></li>
+                            <li><a href="#">Register</a></li>
+                            <li><a href="#"><i class="fa fa-search"></i></a></li>
+                        </ul>                                      
+        </nav>
+                
+            </div>
+        </div>
+    </div>
+</header>
---
+++ med-2/index.php
@@ -0,0 +1,212 @@
+<!DOCTYPE html>
+<html>
+    <?php include './headurl.php';?>
+    <body>
+        <div id="myModalpromo" class="modal fade">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+            </div>
+            <div class="modal-body">
+                <img src="images/promo.jpg" alt="" class="img-fluid">
+            </div>
+        </div>
+    </div>
+</div>
+        <?php include './header.php';?>
+        <section class="hero text-center">
+            <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+                <div class="carousel-inner">
+                    <div class="carousel-item active">
+                        <img src="images/hero1.jpg" class="img-fluid">
+                    </div>
+                    <div class="carousel-item">
+                        <img src="images/hero2.jpg" class="img-fluid">
+                    </div>
+                    <div class="carousel-item">
+                        <img src="images/hero3.jpg" class="img-fluid">
+                    </div>
+                </div>
+                <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span class="sr-only">Previous</span>
+                </a>
+                <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span class="sr-only">Next</span>
+                </a>
+            </div>
+        </section>
+        
+        <section class="icon-sec wow fadeInDown" data-wow-suration="3s">
+            <div class="container">
+                <div class="row clearfix">
+                    <div class="col-md-12">
+                        <h1 class="text-center mb50 ml3">Lorem ipsum dolor</h1>
+                        <ul class="list-inline text-center services-brief">
+                            <li class="list-inline-item" style="filter: grayscale(0) blur(0px);"><a href="#!" class="pulse"><img src="images/icons/01.png" alt=""><span>Convallis</span></a></li>
+                    <li class="list-inline-item" style="filter: grayscale(0) blur(0px);"><a href="#!" class="pulse"><img src="images/icons/02.png" alt=""><span>Quisque</span></a></li>
+                <li class="list-inline-item" style="filter: grayscale(0) blur(0px);"><a href="#!" class="pulse"><img src="images/icons/03.png" alt=""><span>Fermentum</span></a></li>
+                <li class="list-inline-item" style="filter: grayscale(0) blur(0px);"><a href="#!" class="pulse"><img src="images/icons/06.png" alt=""><span>Facilisis</span></a></li>
+                <li class="list-inline-item" style="filter: grayscale(0) blur(0px);"><a href="#!" class="pulse"><img src="images/icons/07.png" alt=""><span>Consectetur</span></a></li>
+            </ul>
+                    </div>
+                </div>
+            </div>
+        </section>
+        
+        <section class="introduction">
+            <div class="container">
+                <div class="row clearfix">
+                    <div class="col-lg-6 wow bounceInLeft" data-wow-offset="300">
+                        <h2 class="mb30">Upload Your Prescription</h2>
+                        <h4 class="sub-title">Lorem Ipsum has been the<br>
+                            industry's standard</h4>
+                        <p class="mb30">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec convallis lacus libero, at semper odio porttitor at. Phasellus ultrices ligula et fermentum mollis. Sed enim urna, tristique in turpis gravida, tempor fermentum nibh. Quisque nec aliquet ante. Ut vel egestas est. Nunc facilisis leo vitae ante consectetur, ut fringilla orci luctus. </p>
+                        <a href="about.php" class="hvr-sweep-to-right">Upload Prescription <i class="fa fa-medkit"></i></a>
+                    </div>
+                    <div class="col-lg-6 wow bounceInRight" data-wow-offset="300">
+                        <img src="images/1.jpg" alt="" class="img-fluid brd-10">
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section class="cta">
+            <div class="container">
+                <div class="row clearfix">
+                    <div class="col-lg-8 offset-lg-2 text-center">
+                        <h3 class="wow slideInRight">Phasellus gravida lacinia velit sit amet posuere
+                            Aliquam erat volutpat</h3>
+                        <a href="#" class="hvr-sweep-to-right wow slideInLeft">Post Your Suggestion</a>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section class="appointment">
+            <div class="container">
+                <div class="row clearfix">
+                    <div class="col-lg-6 wow bounceInLeft" data-wow-offset="300">
+                        <img src="images/2.jpg" alt="" class="img-fluid brd-10">
+                    </div>
+                    <div class="col-lg-6 wow bounceInRight" data-wow-offset="300">
+                        <h2 class="mb30">Get Appointment</h2>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec convallis lacus libero, at semper odio porttitor at. Phasellus ultrices ligula et fermentum mollis. Sed enim urna, tristique in turpis gravida, tempor fermentum nibh. Quisque nec aliquet ante. Ut vel egestas est. Nunc facilisis leo vitae ante consectetur, ut fringilla orci luctus. </p>
+                        <p class="mb30">Ut vel egestas est. Nunc facilisis leo vitae ante consectetur, ut fringilla orci luctus. </p>
+                        <a href="about.php" class="hvr-sweep-to-right">Get Appointment <i class="fa fa-calendar"></i></a>
+                    </div>
+                </div>
+            </div>
+        </section>
+        
+             
+        <section class="assistant-slider">
+            <div class="container">
+                <div class="row clearfix">
+                    <div class="col-lg-12">
+                        <div class="text-center mb50 wow fadeInDown">
+                            <h2>May I Help You</h2>
+                    <p class="lead">Our Assistant boy is here to help you</p>
+                    </div>
+                    <div class="assistant-container slider">                        
+                    <div class="assistant-item">
+                        <div class="item-image">
+                            <a href="#" class="rounded-circle">
+                                <img src="images/d1.jpg" class="img-fluid" alt="">
+                            </a>
+                        </div>
+                        <div class="item-detail">
+                            <ul class="detail">
+                                <li class="assistant-name"> BiraBhadra Dash</li>
+                                <li><strong>Location:</strong> Cuttack</li>
+                                <li><strong>Time:</strong> 9:00pm - 5:00am</li>
+                                <li><span class="price">$2/Hour</span></li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="assistant-item">
+                        <div class="item-image">
+                            <a href="#" class="rounded-circle">
+                                <img src="images/d2.jpg" class="img-fluid" alt="">
+                            </a>
+                        </div>
+                        <div class="item-detail">
+                            <ul class="detail">
+                                <li class="assistant-name"> BiraBhadra Dash</li>
+                                <li><strong>Location:</strong> Cuttack</li>
+                                <li><strong>Time:</strong> 9:00pm - 5:00am</li>
+                                <li><span class="price">$2/Hour</span></li>
+                            </ul>
+                        </div>
+                    </div>
+                        <div class="assistant-item">
+                        <div class="item-image">
+                            <a href="#" class="rounded-circle">
+                                <img src="images/d1.jpg" class="img-fluid" alt="">
+                            </a>
+                        </div>
+                        <div class="item-detail">
+                            <ul class="detail">
+                                <li class="assistant-name"> BiraBhadra Dash</li>
+                                <li><strong>Location:</strong> Cuttack</li>
+                                <li><strong>Time:</strong> 9:00pm - 5:00am</li>
+                                <li><span class="price">$2/Hour</span></li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="assistant-item">
+                        <div class="item-image">
+                            <a href="#" class="rounded-circle">
+                                <img src="images/d2.jpg" class="img-fluid" alt="">
+                            </a>
+                        </div>
+                        <div class="item-detail">
+                            <ul class="detail">
+                                <li class="assistant-name"> BiraBhadra Dash</li>
+                                <li><strong>Location:</strong> Cuttack</li>
+                                <li><strong>Time:</strong> 9:00pm - 5:00am</li>
+                                <li><span class="price">$2/Hour</span></li>
+                            </ul>
+                        </div>
+                    </div>
+                        <div class="assistant-item">
+                        <div class="item-image">
+                            <a href="#" class="rounded-circle">
+                                <img src="images/d1.jpg" class="img-fluid" alt="">
+                            </a>
+                        </div>
+                        <div class="item-detail">
+                            <ul class="detail">
+                                <li class="assistant-name"> BiraBhadra Dash</li>
+                                <li><strong>Location:</strong> Cuttack</li>
+                                <li><strong>Time:</strong> 9:00pm - 5:00am</li>
+                                <li><span class="price">$2/Hour</span></li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="assistant-item">
+                        <div class="item-image">
+                            <a href="#" class="rounded-circle">
+                                <img src="images/d2.jpg" class="img-fluid" alt="">
+                            </a>
+                        </div>
+                        <div class="item-detail">
+                            <ul class="detail">
+                                <li class="assistant-name"> BiraBhadra Dash</li>
+                                <li><strong>Location:</strong> Cuttack</li>
+                                <li><strong>Time:</strong> 9:00pm - 5:00am</li>
+                                <li><span class="price">$2/Hour</span></li>
+                            </ul>
+                        </div>
+                    </div>
+                    
+                                 
+                    </div>
+                    </div>
+                </div>
+            </div>
+  </section>
+        
+        
+        <?php include './footer.php';?>
+</html>
